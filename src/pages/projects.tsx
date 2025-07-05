9d1dda60d686d88a7f201a112bd36b591ee04231
KamijoToma <admin@misakacloud.net>
2025-07-05 17:33:01 +0000

    feat(projects): add some new projects from github.com/revyos
    Signed-off-by: KamijoToma <admin@misakacloud.net>

--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -15,48 +15,68 @@ const Intro = () => (
 
 const projects: InfoCardProps[] = [
   {
-    title: 'th1520-linux-kernel',
-    title_en: 'th1520-linux-kernel',
+    title: 'TH1520 Linux 内核',
+    title_en: 'TH1520 Linux kernel',
     logo: '/img/github-mark.svg',
     subtitle: 'revyos/th1520-linux-kernel',
     subtitle_en: 'revyos/th1520-linux-kernel',
-    description: 'TH1520 的 Linux kernel',
+    description: '适用于 TH1520 芯片的 Linux kernel',
     description_en: 'Linux kernel for TH1520',
     link: 'https://github.com/revyos/th1520-linux-kernel'
   },
   {
-    title: 'th1520-boot-firmware',
-    title_en: 'th1520-boot-firmware',
+    title: 'TH1520 Boot Firmware',
+    title_en: 'TH1520 Boot Firmware',
     logo: '/img/github-mark.svg',
     subtitle: 'revyos/th1520-boot-firmware',
     subtitle_en: 'revyos/th1520-boot-firmware',
-    description: 'TH1520 的 boot firmware',
+    description: '适用于 TH1520 芯片的 boot firmware',
     description_en: 'Boot firmware for TH1520',
     link: 'https://github.com/revyos/th1520-boot-firmware'
   },
   {
-    title: 'RevyOS anheng-image',
-    title_en: 'RevyOS anheng-image',
+    title: 'SG2044 Linux 内核',
+    title_en: 'SG2044 Linux kernel',
     logo: '/img/github-mark.svg',
-    subtitle: 'revyos/anheng-image',
-    subtitle_en: 'revyos/anheng-image',
-    description: 'RevyOS 安恒镜像构建脚本',
-    description_en: 'RevyOS anheng-image build scripts',
-    link: 'https://github.com/revyos/anheng-image'
+    subtitle: 'revyos/sg2044-vendor-kernel',
+    subtitle_en: 'revyos/sg2044-vendor-kernel',
+    description: '适用于 SG2044 芯片的 Linux kernel',
+    description_en: 'Linux kernel for SG2044',
+    link: 'https://github.com/revyos/sg2044-vendor-kernel'
   },
   {
-    title: 'RevyOS anheng-profiles',
-    title_en: 'RevyOS anheng-profiles',
+    title: 'SG2042 Linux 内核',
+    title_en: 'SG2042 Linux kernel',
     logo: '/img/github-mark.svg',
-    subtitle: 'revyos/anheng-profiles',
-    subtitle_en: 'revyos/anheng-profiles',
-    description: 'RevyOS 安恒设备支持包',
-    description_en: 'RevyOS anheng-profiles device support package',
-    link: 'https://github.com/revyos/anheng-profiles'
+    subtitle: 'revyos/sg2042-vendor-kernel',
+    subtitle_en: 'revyos/sg2042-vendor-kernel',
+    description: '适用于 SG2042 芯片的 Linux kernel',
+    description_en: 'Linux kernel for SG2042',
+    link: 'https://github.com/revyos/sg2042-vendor-kernel'
   },
   {
-    title: 'RevyOS (本站)',
-    title_en: 'RevyOS (This Site)',
+    title: 'TH1520 系统镜像构建脚本',
+    title_en: 'TH1520 System Image Build Scripts',
+    logo: '/img/github-mark.svg',
+    subtitle: 'revyos/mkimg-th1520',
+    subtitle_en: 'revyos/mkimg-th1520',
+    description: 'TH1520 系统镜像构建脚本',
+    description_en: 'System image build scripts for TH1520',
+    link: 'https://github.com/revyos/mkimg-th1520'
+  },
+  {
+    title: 'SG2042 系统镜像构建脚本',
+    title_en: 'SG2042 System Image Build Scripts',
+    logo: '/img/github-mark.svg',
+    subtitle: 'revyos/mkimg-sg2042',
+    subtitle_en: 'revyos/mkimg-sg2042',
+    description: 'SG2042 系统镜像构建脚本',
+    description_en: 'System image build scripts for SG2042',
+    link: 'https://github.com/revyos/mkimg-sg2042'
+  },
+  {
+    title: 'RevyOS 文档站',
+    title_en: 'RevyOS Docs',
     logo: '/img/github-mark.svg',
     subtitle: 'revyos/revyos-docs',
     subtitle_en: 'revyos/revyos-docs',
